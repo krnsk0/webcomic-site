308da7df755c7dec19e3efc1fdfeec0982501e4b
Jon Kurinsky <jkurinsky@gmail.com>
2019-10-19 10:09:21 -0400

    configure json loading for cms

--- blog/test.md
+++
@@ -1,6 +0,0 @@
----
-path: test
-date: 2019-10-19T12:37:00.521Z
-title: test
----
-test
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./comics`,
+      },
+    },
+  ],
 }
