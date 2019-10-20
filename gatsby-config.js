ac7deb7a3902009e54da203d7d6c05edcdbe69c5
Jon Kurinsky <jkurinsky@gmail.com>
2019-10-20 10:10:42 -0400

    set up pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,20 @@ module.exports = {
         path: `./data`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Awakened`,
+        short_name: `Awakened`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/chapters.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+
+export default props => {
+  const chapters = props.data.allChaptersJson.edges
+  return (
+    <div>
+      <div>
+        <b>Chapters</b>
+      </div>
+      <div>
+        {chapters.map(chapter => {
+          const { number, title, page_number } = chapter.node
+          console.log("number: ", number)
+          return (
+            <div key={number}>
+              <Link to={`/page/${page_number}`}>
+                Chapter {number}: {title}
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allChaptersJson {
+      edges {
+        node {
+          number
+          page_number
+          title
+        }
+      }
+    }
+  }
+`
