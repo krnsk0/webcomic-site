d4f2f1b5be23df95634f676910ae1dd3dbfb02cc
Jon Kurinsky <jkurinsky@gmail.com>
2019-10-20 16:18:33 -0400

    reconfigure createPages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
-    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,43 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const pageTemplate = path.resolve(`src/templates/page.js`)
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const pageTemplate = path.resolve(`src/templates/page.js`)
 
-  return graphql(
-    `
-      query loadPagesQuery($limit: Int!) {
-        allPagesJson(limit: $limit) {
-          edges {
-            node {
-              image
-              date
-              body
-              page_number
-            }
-          }
-        }
-      }
-    `,
-    { limit: 1000 }
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+//   return graphql(
+//     `
+//       query loadPagesQuery($limit: Int!) {
+//         allPagesJson(limit: $limit) {
+//           edges {
+//             node {
+//               image
+//               date
+//               body
+//               page_number
+//             }
+//           }
+//         }
+//       }
+//     `,
+//     { limit: 1000 }
+//   ).then(result => {
+//     if (result.errors) {
+//       throw result.errors
+//     }
 
-    // Create blog post pages.
-    result.data.allPagesJson.edges.forEach(({ node }) => {
-      createPage({
-        // Path for this page — required
-        path: `/page/${node.page_number}`,
-        component: pageTemplate,
-        context: {
-          image: node.image,
-          date: node.date,
-          body: node.body,
-          page_number: node.page_number,
-        },
-      })
-    })
-  })
-}
+//     // Create blog post pages.
+//     result.data.allPagesJson.edges.forEach(({ node }) => {
+//       createPage({
+//         // Path for this page — required
+//         path: `/page/${node.page_number}`,
+//         component: pageTemplate,
+//         context: {
+//           image: node.image,
+//           date: node.date,
+//           body: node.body,
+//           page_number: node.page_number,
+//         },
+//       })
+//     })
+//   })
+// }
