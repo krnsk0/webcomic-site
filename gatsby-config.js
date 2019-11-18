6d07053884355483cf7f21a9f4b0bbe083a3763f
Jon Kurinsky <jkurinsky@gmail.com>
2019-11-18 18:16:01 -0500

    set up analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,11 @@ module.exports = {
         })),
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-317656-3",
+      },
+    },
   ],
 }
